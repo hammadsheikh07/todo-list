a7184639a54a34b9582cd865752503f66317fcb2
Muhammad Hammad Shahid <hammad.shahid.pk@gmail.com>
2022-08-13 21:45:10 +0500

    ui changes (responsiveness error)

--- src/App.js
+++ src/App.js
@@ -38,165 +38,167 @@ const App = () => {
         alignItems="center"
         justifyContent="center"
       >
-        <Box
-          mt={3}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          mx="auto"
-        >
-          <Typography variant="h1" sx={{ textAlign: "center" }}>
-            Todo list
-          </Typography>
-          <form onSubmit={(e) => addwork(e)} ref={ref}>
+        <Box mx="auto">
+          <Box
+            mt={3}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            mx="auto"
+          >
+            <Typography variant="h1" sx={{ textAlign: "center" }}>
+              Todo list
+            </Typography>
+            <form onSubmit={(e) => addwork(e)} ref={ref}>
+              <Box
+                mt={3}
+                px={10}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <TextField
+                  onChange={(e) => changeWork(e.target.value)}
+                  multiline
+                  maxRows={3}
+                  value={newWork}
+                  sx={{ minWidth: "300px", backgroundColor: "white" }}
+                  label="Enter Your Work"
+                ></TextField>
+                <Button sx={{ ml: 2 }} variant="contained" type="submit">
+                  Add
+                </Button>
+              </Box>
+            </form>
+          </Box>
+          <Box
+            display="flex"
+            mx="auto"
+            alignItems="center"
+            justifyContent="center"
+            py={4}
+            sx={{ flexWrap: "wrap" }}
+          >
             <Box
-              mt={3}
-              px={10}
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
+              mx={2}
+              p={1}
+              mt={4}
+              px={3}
+              sx={{
+                width: "330px",
+                height: "350px",
+                backgroundColor: "white",
+                boxShadow: "0 0 10px rgba(0,0,0,0.33)",
+                borderRadius: "10px",
+                overflowY: "scroll",
+              }}
             >
-              <TextField
-                onChange={(e) => changeWork(e.target.value)}
-                multiline
-                maxRows={3}
-                value={newWork}
-                sx={{ minWidth: "300px", backgroundColor: "white" }}
-                label="Enter Your Work"
-              ></TextField>
-              <Button sx={{ ml: 2 }} variant="contained" type="submit">
-                Add
-              </Button>
+              {containerOne.map((entry, index) => (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  mt={2}
+                  justifyContent="space-between"
+                  sx={{
+                    backgroundColor: "#f2f2f2",
+                    boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
+                    borderRadius: "3px",
+                  }}
+                >
+                  <Box>
+                    <Typography variant="body1">{entry.data}</Typography>
+                  </Box>
+                  <Box>
+                    <Button>&#60;</Button>
+                    <Button onClick={(e) => handleOneright(entry.data, index)}>
+                      &#62;
+                    </Button>
+                  </Box>
+                </Box>
+              ))}
             </Box>
-          </form>
-        </Box>
-        <Box
-          display="flex"
-          mx="auto"
-          alignItems="center"
-          justifyContent="center"
-          py={4}
-          sx={{ flexWrap: "wrap" }}
-        >
-          <Box
-            mx={2}
-            p={1}
-            mt={4}
-            px={3}
-            sx={{
-              width: "330px",
-              height: "350px",
-              backgroundColor: "white",
-              boxShadow: "0 0 10px rgba(0,0,0,0.33)",
-              borderRadius: "10px",
-              overflowY: "scroll",
-            }}
-          >
-            {containerOne.map((entry, index) => (
-              <Box
-                display="flex"
-                alignItems="center"
-                p={1}
-                mt={2}
-                justifyContent="space-between"
-                sx={{
-                  backgroundColor: "#f2f2f2",
-                  boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
-                  borderRadius: "3px",
-                }}
-              >
-                <Box>
-                  <Typography variant="body1">{entry.data}</Typography>
+            <Box
+              mx={2}
+              p={1}
+              mt={4}
+              px={3}
+              sx={{
+                width: "330px",
+                height: "350px",
+                backgroundColor: "white",
+                boxShadow: "0 0 10px rgba(0,0,0,0.33)",
+                borderRadius: "10px",
+                overflowY: "scroll",
+              }}
+            >
+              {containerTwo.map((entry, index) => (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  mt={2}
+                  justifyContent="space-between"
+                  sx={{
+                    backgroundColor: "#f2f2f2",
+                    boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
+                    borderRadius: "3px",
+                  }}
+                >
+                  <Box>
+                    <Typography variant="body1">{entry.data}</Typography>
+                  </Box>
+                  <Box>
+                    <Button onClick={(e) => handleTwoleft(entry.data, index)}>
+                      &#60;
+                    </Button>
+                    <Button onClick={(e) => handleTworight(entry.data, index)}>
+                      &#62;
+                    </Button>
+                  </Box>
                 </Box>
-                <Box>
-                  <Button>&#60;</Button>
-                  <Button onClick={(e) => handleOneright(entry.data, index)}>
-                    &#62;
-                  </Button>
+              ))}
+            </Box>
+            <Box
+              mx={2}
+              py={1}
+              px={3}
+              mt={4}
+              sx={{
+                width: "330px",
+                height: "350px",
+                backgroundColor: "white",
+                boxShadow: "0 0 10px rgba(0,0,0,0.33)",
+                borderRadius: "10px",
+                overflowY: "scroll",
+              }}
+            >
+              {containerThree.map((entry, index) => (
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  mt={2}
+                  justifyContent="space-between"
+                  sx={{
+                    backgroundColor: "#f2f2f2",
+                    boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
+                    borderRadius: "3px",
+                  }}
+                >
+                  <Box>
+                    <Typography variant="body1">{entry.data}</Typography>
+                  </Box>
+                  <Box>
+                    <Button onClick={(e) => handleThreeleft(entry.data, index)}>
+                      &#60;
+                    </Button>
+                    <Button>&#62;</Button>
+                  </Box>
                 </Box>
-              </Box>
-            ))}
-          </Box>
-          <Box
-            mx={2}
-            p={1}
-            mt={4}
-            px={3}
-            sx={{
-              width: "330px",
-              height: "350px",
-              backgroundColor: "white",
-              boxShadow: "0 0 10px rgba(0,0,0,0.33)",
-              borderRadius: "10px",
-              overflowY: "scroll",
-            }}
-          >
-            {containerTwo.map((entry, index) => (
-              <Box
-                display="flex"
-                alignItems="center"
-                p={1}
-                mt={2}
-                justifyContent="space-between"
-                sx={{
-                  backgroundColor: "#f2f2f2",
-                  boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
-                  borderRadius: "3px",
-                }}
-              >
-                <Box>
-                  <Typography variant="body1">{entry.data}</Typography>
-                </Box>
-                <Box>
-                  <Button onClick={(e) => handleTwoleft(entry.data, index)}>
-                    &#60;
-                  </Button>
-                  <Button onClick={(e) => handleTworight(entry.data, index)}>
-                    &#62;
-                  </Button>
-                </Box>
-              </Box>
-            ))}
-          </Box>
-          <Box
-            mx={2}
-            py={1}
-            px={3}
-            mt={4}
-            sx={{
-              width: "330px",
-              height: "350px",
-              backgroundColor: "white",
-              boxShadow: "0 0 10px rgba(0,0,0,0.33)",
-              borderRadius: "10px",
-              overflowY: "scroll",
-            }}
-          >
-            {containerThree.map((entry, index) => (
-              <Box
-                display="flex"
-                alignItems="center"
-                p={1}
-                mt={2}
-                justifyContent="space-between"
-                sx={{
-                  backgroundColor: "#f2f2f2",
-                  boxShadow: "0 0 10px rgba(1, 51, 125,0.22)",
-                  borderRadius: "3px",
-                }}
-              >
-                <Box>
-                  <Typography variant="body1">{entry.data}</Typography>
-                </Box>
-                <Box>
-                  <Button onClick={(e) => handleThreeleft(entry.data, index)}>
-                    &#60;
-                  </Button>
-                  <Button>&#62;</Button>
-                </Box>
-              </Box>
-            ))}
+              ))}
+            </Box>
           </Box>
         </Box>
       </Box>
